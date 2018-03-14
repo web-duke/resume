8154b75f7f95da8707fce2303699256ad04619d9
g.marques <g.marques@idiliz.com>
2018-03-14 15:51:46 +0100

    skills

--- src/app/skills/skills-item/skills-item.component.ts
+++ src/app/skills/skills-item/skills-item.component.ts
@@ -9,10 +9,18 @@ export class SkillsItemComponent implements OnInit {
   @Input() label = ''; // skill label
   @Input() xp = 0; // skill percentage
   @Input() color = ''; // skill color
+  @Input() imgName = ''; // skill image name
+  @Input() imgPath = 'assets/img/'; // skill image path
 
   constructor() { }
 
   ngOnInit() {
+    this.imgPath = this.imgPath + this.imgName;
   }
+
+  // getImgPath() {
+  //   this.imgPath = this.imgPath + this.imgName;
+  //   return this.imgPath;
+  // }
 
 }
