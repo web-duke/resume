6d2247c5b86470cca92b33d975bebc34f355fd01
g.marques <g.marques@idiliz.com>
2018-03-15 17:11:50 +0100

    work xp

--- src/app/skills/skills-item/skills-item.component.ts
+++ src/app/skills/skills-item/skills-item.component.ts
@@ -10,7 +10,7 @@ export class SkillsItemComponent implements OnInit {
   @Input() xp = 0; // skill percentage
   @Input() color = ''; // skill color
   @Input() imgName = ''; // skill image name
-  @Input() imgPath = 'assets/img/'; // skill image path
+  @Input() imgPath = 'assets/img/'; // image path
   @Input() srcUrl = '#'; // skill source url
 
   constructor() { }
--- src/app/work-experience/work-experience-item/work-experience-item.component.ts
+++ src/app/work-experience/work-experience-item/work-experience-item.component.ts
@@ -8,11 +8,15 @@ import { Component, OnInit, Input } from '@angular/core';
 export class WorkExperienceItemComponent implements OnInit {
   @Input() title = ''; // job title
   @Input() company = ''; // company name
+  @Input() imgName = ''; // company logo
+  @Input() imgPath = 'assets/img/'; // image path
+  @Input() srcUrl = '#'; // company source url
   @Input() dates = ''; // dates of work
 
   constructor() { }
 
   ngOnInit() {
+    this.imgPath = this.imgPath + this.imgName;
   }
 
 }
